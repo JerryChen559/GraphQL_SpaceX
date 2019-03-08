79677e4a422f615fc8b0921a82860a9e9377d109
Jerry Chen <zgchen559@gmail.com>
2019-03-07 22:41:47 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const express = require("express");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    // graphiql: uses our client to make queries to our server
+    graphiql: true
+  })
+);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
