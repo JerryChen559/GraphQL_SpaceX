48cbe885e32cf0211735788fe3083585d1471218
Jerry Chen <zgchen559@gmail.com>
2019-03-08 14:18:08 -0800

    built schema

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-const express = require("express");
+const schema = require("./schema");
 
 const app = express();
 
